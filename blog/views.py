230481589955d62bc4ff7d477525e7f18d775706
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2023-06-13 21:09:11 +0100

    Adding urls and templates to the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 
 def post_list(request):
@@ -13,10 +12,11 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No Post found.')
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
 
     return render(
         request,
