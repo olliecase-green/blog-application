bbc89a6b1f043d1d62a811067e97ec1314fd4678
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2023-10-09 22:23:54 +0100

    Removing post_list function view which was replaced by class view

--- blog/views.py
+++ blog/views.py
@@ -13,26 +13,6 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
-
-
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
