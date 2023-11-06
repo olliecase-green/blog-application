386cd3fcd7a629459680dc6eb04978599f4f7089
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2023-11-06 21:53:30 +0000

    Adding share posts by email feature

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
+    # Post views
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail, name='post_detail')
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'
+    ),
+    path('<int:post_id>/share/', views.post_share, name='post_share')
 ]
