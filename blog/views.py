265ca2fd31e8782508f565805c0d835f465e38ea
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-02-08 09:53:06 +0000

    Adding comment imports to views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
