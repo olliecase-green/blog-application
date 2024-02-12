43a51efa58471709718cf7bf68cb7713467af511
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-02-12 23:02:36 +0000

    Removed unused import

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
