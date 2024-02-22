c84b28e566fba3638b24370201f7e8be876a9dc3
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-02-22 16:55:30 +0000

    Changing post_list to function view

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,30 @@
 from .models import Post
 from .forms import EmailPostForm, CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
 
-class PostListView(ListView):
-    # Alternative post list view
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request):
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
 
 
 def post_detail(request, year, month, day, post):
