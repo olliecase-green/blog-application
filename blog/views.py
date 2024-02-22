ddcc95ebd603e7569d250f8ec068977003404d0f
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-02-22 17:15:18 +0000

    Adding tags for posts using django-taggit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from .models import Post
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -7,8 +8,13 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -23,7 +29,10 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {
+            'posts': posts,
+            'tag': tag
+        }
     )
 
 
