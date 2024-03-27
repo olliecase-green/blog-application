c4d964be4099b03577005cf2665bf05a5dd5c91f
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-03-27 13:42:48 +0000

    Creating search form and adding to views

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 
 def post_list(request, tag_slug=None):
@@ -134,3 +135,30 @@ def post_comment(request, post_id):
             'comment': comment
         }
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post
+                .published
+                .annotate(search=SearchVector('title', 'body'))
+                .filter(search=query)
+            )
+
+        return render(
+            request,
+            'blog/post/search.html',
+            {
+                'form': form,
+                'query': query,
+                'results': results
+            }
+        )
