2966dec770aca98e2cddecd67fe7a2ce9be9a964
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2024-03-27 13:10:27 +0000

    Changing to postgres database

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'taggit',
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': config('BLOG_PASSWORD')
     }
 }
 
